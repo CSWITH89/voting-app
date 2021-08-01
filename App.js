f5978bc493770e8eb552292875617b1661c4035a
Chris Withall <chriswithall@Chriss-MacBook-Pro-2.local>
2021-08-01 16:52:37 +0200

    LOGIC setup state in Context

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {createContext, useState} from 'react';
 import MainNav from './src/navigation/MainNav';
 import {NavigationContainer} from '@react-navigation/native';
+import DummyData from './src/consts/DummyData';
+
+const DataContext = createContext();
 
 const App = () => {
+  const [gameData, setGameData] = useState(
+    DummyData.map(game => {
+      return {...game, count: 0};
+    }),
+  );
+
   return (
-    <NavigationContainer>
-      <MainNav></MainNav>
-    </NavigationContainer>
+    <DataContext.Provider value={{gameData, setGameData}}>
+      <NavigationContainer>
+        <MainNav></MainNav>
+      </NavigationContainer>
+    </DataContext.Provider>
   );
 };
 
